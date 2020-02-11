1518557f712fb8f6579e18fb3bd009f4d063ba25
George Gruner <imgeorge220@gmail.com>
2020-02-11 15:52:29 -0800

    Step 0/1

--- markov.js
+++ markov.js
@@ -1,5 +1,7 @@
 /** Textual markov chain generator */
 
+let _ = require('lodash');
+
 
 class MarkovMachine {
 
@@ -8,7 +10,7 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
-    this.makeChains();
+    this.chains = this.makeChains();
   }
 
   /** set markov chains:
@@ -17,13 +19,43 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let result = {};
+
+    for (let index in this.words) {
+      let nextWord = this.words[+index + 1] || null;
+
+      if (result[this.words[index]]) {
+        result[this.words[index]].push(nextWord);
+      } else {
+        result[this.words[index]] = [nextWord];
+      }
+    }
+
+    return result;
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let count = 0;
+    let currentWord = _.sample(this.words);
+    let text = currentWord;
+    let nextWord;
+
+    while (count < numWords) {
+      nextWord = _.sample(this.chains[currentWord]);
+      
+      if (!nextWord) {
+        return text;
+      };
+      
+      text += ` ${nextWord}`;
+      currentWord = nextWord;
+      count++;
+    }
+
+    return text;
   }
 }
+let mm = new MarkovMachine(text);
